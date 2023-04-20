16226e1e35f3a75aa841530eb3168bd940219bb9
sameersyedtauqeer <syedsameerali26@gmail.com>
2023-04-20 15:52:20 +0500

    first commit

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ if (process.env.NODE_ENV == "production") {
     const path = require('path')
     app.get("/", (req, res) => {
         app.use(express.static(path.resolve(__dirname, ('client', 'build'))))
-        res, sendFile(path.resolve(__dirname, ('client', 'build', 'index.html')))
+        res.sendFile(path.resolve(__dirname, ('client', 'build', 'index.html')))
     })
 }
 app.listen(PORT, () => {
